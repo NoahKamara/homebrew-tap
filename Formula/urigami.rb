6a9a6f58cee08e05aa722a76cc798787419f378e
Noah Kamara <mail@noahkamara.com>
2025-01-27 19:12:48 +0100

    update(urigami): v0.1.1 build

--- Formula/urigami.rb
+++ Formula/urigami.rb
@@ -1,24 +1,19 @@
 class Urigami < Formula
   desc "a command-line utility for managing default applications on macOS"
   homepage "https://github.com/NoahKamara/Urigami"
-  url "https://github.com/NoahKamara/Urigami/releases/download/v0.1.1/v0.1.1.tar.gz"
-  sha256 "8e38e71025f611ad0a7c49cefafa9562c21990577db57ab5bc1e2ae116dd19df"
+  url "https://github.com/NoahKamara/Urigami/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "11351401ac9d5f3b9c743f7d71e3dd0f040f4719e1d6a303ededb61eeae8a395"
   version "v0.1.1"
 
   def install
-    bin.install "urigami"
-    
+    # Build
+    system "swift", "build", "-c", "release", "--disable-sandbox", "--arch", "arm64", "--arch", "x86_64"
+
+    # Install
+    bin.install ".build/apple/Products/Release/urigami"
+
     # Generate and install completions
-    bash_output = Utils.safe_popen_read("#{bin}/urigami", "--generate-completion-script", "bash")
-    (bash_completion/"urigami").write bash_output
-
-    zsh_output = Utils.safe_popen_read("#{bin}/urigami", "--generate-completion-script", "zsh")
-    (zsh_completion/"urigami").write zsh_output
-
-    fish_output = Utils.safe_popen_read("#{bin}/urigami", "--generate-completion-script", "fish")
-    (fish_completion/"urigami.fish").write fish_output
-
-    bin.install "urigami"
+    generate_completions_from_executable(bin/"urigami", "--generate-completion-script")
   end
 
   test do
