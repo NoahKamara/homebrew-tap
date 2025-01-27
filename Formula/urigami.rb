cbcd665c56e260606dfa78142c2eba1778cfd42d
Noah Kamara <mail@noahkamara.com>
2025-01-27 01:53:27 +0100

    update(urigami): v0.1.1

--- Formula/urigami.rb
+++ Formula/urigami.rb
@@ -1,11 +1,23 @@
 class Urigami < Formula
   desc "a command-line utility for managing default applications on macOS"
   homepage "https://github.com/NoahKamara/Urigami"
-  url "https://github.com/NoahKamara/Urigami/releases/download/v0.1.0/v0.1.0.tar.gz"
-  sha256 "779a7f7ba7d1f555debcbffb4583cbf099a978d67c10ed788e6030d7343e36a2"
-  version "v0.1.0"
+  url "https://github.com/NoahKamara/Urigami/releases/download/v0.1.1/v0.1.1.tar.gz"
+  sha256 "8e38e71025f611ad0a7c49cefafa9562c21990577db57ab5bc1e2ae116dd19df"
+  version "v0.1.1"
 
   def install
+    bin.install "urigami"
+    
+    # Generate and install completions
+    bash_output = Utils.safe_popen_read("#{bin}/urigami", "--generate-completion-script", "bash")
+    (bash_completion/"urigami").write bash_output
+
+    zsh_output = Utils.safe_popen_read("#{bin}/urigami", "--generate-completion-script", "zsh")
+    (zsh_completion/"urigami").write zsh_output
+
+    fish_output = Utils.safe_popen_read("#{bin}/urigami", "--generate-completion-script", "fish")
+    (fish_completion/"urigami.fish").write fish_output
+
     bin.install "urigami"
   end
 
